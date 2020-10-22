f76ed7a830d713822824d4ceda5096dd1e60f900
Oliver <avilebroker@gmail.com>
2020-10-22 01:44:20 -0500

    styling the rest of the page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Provider } from "jotai";
 
+import styled from "styled-components";
+
 import Scene from "./Scene";
 import ObjectTypePicker from "./webComponents/ObjectTypePicker";
 import MapControls from "./webComponents/MapControls";
 
 import Header from "./webComponents/Header";
 import Tutorial from "./webComponents/Tutorial";
+
+const PageContainer = styled.div`
+  max-width: 70rem;
+  width: calc(100% - 4rem);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default () => (
   <Provider>
@@ -16,7 +25,9 @@ export default () => (
     <MapControls />
 
     <Tutorial />
-
-    <h1>What is Kurb anyway?</h1>
+    <PageContainer>
+      <h1>What's a Qurb anyway?</h1>
+      <p>Nothing! It's all made up.</p>
+    </PageContainer>
   </Provider>
 );
